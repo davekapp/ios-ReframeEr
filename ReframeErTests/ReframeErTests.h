674304320b0174b667083944988da655d17a93e2
David Kapp <davidkapp@coshx.com>
2012-02-09 18:37:31 -0700

    Initial Commit

---
+++ ReframeErTests/ReframeErTests.h
@@ -0,0 +1,13 @@
+//
+//  ReframeErTests.h
+//  ReframeErTests
+//
+//  Created by David Kapp on 2/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ReframeErTests : SenTestCase
+
+@end
---
+++ ReframeErTests/ReframeErTests.m
@@ -0,0 +1,32 @@
+//
+//  ReframeErTests.m
+//  ReframeErTests
+//
+//  Created by David Kapp on 2/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "ReframeErTests.h"
+
+@implementation ReframeErTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ReframeErTests");
+}
+
+@end
